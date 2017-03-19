e8c6708a49730f39cea0e877121bbffbbb48c7f8
Ilia Ivanov <ilya.pskov@gmail.com>
2017-03-19 15:55:45 +0200

    Playing around with middlewares

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,11 +1,39 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
-var app = express();
+var app = express(),
+    maintenanceNode = false;
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public'));
+
+// Middlewares start
+
+app.use((req, res, next) => {
+    var now = new Date().toString(),
+        log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    if (maintenanceNode) {
+        res.render('maintenance');
+    } else {
+        next();
+    }
+});
+
+app.use(express.static(__dirname + '/public')); // <--- This is actually a middleware.
+
+// Middlewares end
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
