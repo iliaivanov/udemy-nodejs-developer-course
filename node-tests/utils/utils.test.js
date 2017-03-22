4665ad96f2e57e15bb880916286808995b99356b
Ilia Ivanov <ilya.pskov@gmail.com>
2017-03-23 00:14:01 +0200

    Expect lib added. Some nice assertions tested.

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,49 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('should add two numbers', () => {
     var result = utils.add(1, 9);
 
-    if (result != 10) {
-        throw new Error(`Expected 10, but got ${result}`);
-    }
+    expect(result).toBe(10, `Expected 10, but got ${result}`).toBeA('number');
 });
 
 it('should sqare a number', () => {
     var result = utils.square(5);
 
-    if (result != 25) {
-        throw new Error(`Expected 25, but got ${result}`);
-    }
+    expect(result).toBe(25, `Expected 25, but got ${result}`).toBeA('number');
 });
+
+it('set first and last names', () => {
+    var user = {
+        location: 'ee',
+        age: 30
+    };
+
+    var updatedUser = utils.setName(user, 'Ilia Ivanov');
+
+    expect(updatedUser).toInclude({
+        firstName: 'Ilia',
+        lastName: 'Ivanov'
+    });
+});
+
+// Playing around.
+// it('should expect some stuff', () => {
+//     expect(12).toNotBe(13); // Not working with objects.
+//     expect({name: 'Ilia'}).toNotEqual({name: 'Ilia'});
+//     expect({name: 'Ilia'}).toEqual({name: 'Ilia'});
+//     expect([1, 3, 5]).toInclude(1);
+//     expect([1, 3, 5]).toExclude(2);
+//     expect({
+//         name: 'ilia',
+//         age: 30
+//     }).toInclude({
+//         age: 30
+//     });
+//     expect({
+//         name: 'ilia',
+//         age: 30
+//     }).toExclude({
+//         age: 31
+//     });
+// });
