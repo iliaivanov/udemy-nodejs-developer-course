b88ee5db47e2695a242e1551adc79a814865c41b
Ilia Ivanov <ilya.pskov@gmail.com>
2017-04-10 22:28:13 +0300

    Playing around with mongo features.

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
+// Destructuring - ES6 feature.
+// let user = {name: "Ilia", age: 25};
+// let {name} = user;
+// console.log(name);
+
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb'); // Destructuring.
+
+// Generating object ids.
+// let obj = new ObjectID();
+// console.log(obj);
 
 MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
     if (err) {
@@ -18,16 +28,16 @@ MongoClient.connect('mongodb://127.0.0.1:27017/TodoApp', (err, db) => {
     //     console.log(JSON.stringify(result.ops));
     // });
 
-    db.collection('Users').insertOne({
-        name: 'Ilia',
-        age: 30
-    }, (err, result) => {
-        if (err) {
-            return console.log(err);
-        }
-
-        console.log(JSON.stringify(result.ops));
-    });
+    // db.collection('Users').insertOne({
+    //     name: 'Ilia',
+    //     age: 30
+    // }, (err, result) => {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //
+    //     console.log(result.ops[0]._id.getTimestamp());
+    // });
 
     db.close();
 });
