ee2b8848c65e8969a421c5e02f13f5be68de17da
Ilia Ivanov <ilya.pskov@gmail.com>
2017-04-27 21:44:19 +0300

    Add test for DELETE /users/me/token

--- node-todo-api/server/tests/server.test.js
+++ node-todo-api/server/tests/server.test.js
@@ -353,3 +353,26 @@ describe('POST /users/login', () => {
             });
     });
 });
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+        let token = users[0].tokens[0].token;
+
+        request(app)
+            .delete('/users/me/token')
+            .set('x-auth', token)
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[0]._id).then((user) => {
+                    expect(user.tokens.length).toBe(0);
+                    done();
+                }).catch((err) => {
+                    done(err);
+                });
+            });
+    });
+});
