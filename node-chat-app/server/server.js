99e83d7151548e3120a32c9858d44f8df6c4361c
Ilia Ivanov <ilya.pskov@gmail.com>
2017-05-14 17:41:23 +0300

    Greet new user and alert others

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -16,13 +16,33 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'admin',
+        text: 'Welcome to the chat!',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (newMessage) => {
         console.log('New message', newMessage);
+
         io.emit('newMessage', {
             from: newMessage.from,
             text: newMessage.text,
             createdAt: new Date().getTime()
         });
+
+        // Broadcast to everybody but me.
+        // socket.broadcast.emit('newMessage', {
+        //     from: newMessage.from,
+        //     text: newMessage.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 
     socket.on('disconnect', () => {
